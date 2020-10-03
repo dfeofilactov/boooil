4e7a96a835fbdb46227b730473266e4824e950c5
Denis Feofilaktov <denis.feofilactov@gmail.com>
2020-10-03 18:17:47 +0300

    added new boil

---
+++ boils/react-hooks/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../sass/main.sass';
+
+const App = () => (
+    <>
+        <header>
+            <h2>Ract Boil</h2>
+        </header>
+        <main>
+            <p className='description'>Your app here...</p>
+        </main>
+        <footer>
+            Adooone Boooil
+        </footer>
+    </>
+);
+
+export default App;
